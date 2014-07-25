adf5a29bc92cdc07f6bd56a468c125716aa703dd
Scott Ritchie <sritchie73@gmail.com>
2014-07-25 11:28:20 +1000

    added dispatch function for casting an external pointer to a BigMatrix to its appropriate Armadillo matrix

--- src/2014-07-25-using-bigmemory-with-rcpparmadillo.cpp
+++ src/2014-07-25-using-bigmemory-with-rcpparmadillo.cpp
@@ -1,61 +1,72 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 /**
- * @title Armadillo subsetting
- * @author Dirk Eddelbuettel
+ * @title Using RcppArmadillo with
+ * @author Scott Ritchie
  * @license GPL (>= 2)
- * @tags armadillo matrix featured
- * @summary This example shows how to subset Armadillo matrices
- *
- * A [StackOverflow
- * question](http://stackoverflow.com/questions/10212247/conversion-from-armaumat-to-armamat)
- * asked how convert from `arma::umat` to `arma::mat`.  The former is
- * format used for `find` and other logical indexing.
- *
- * For the particular example at hand, a call to the `conv_to`
- * converter provided the solution. We rewrite the answer here using
- * the newer format offered by Rcpp attributes and its `sourceCpp()`
- * function.
+ * @tags armadillo bigmemory matrix
+ * @summary This example shows how to use RcppArmadillo with `big.matrix` objects from the `bigmemory` package.
  *
  */
 
 #include <RcppArmadillo.h>
-// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::depends(RcppArmadillo, BH, bigmemory)]]
 
 using namespace Rcpp ;
 
+// The function which performs the linear algebra you want
+//
+void someArmaFunction(const arma::Mat<T>& aBigMat) {
+  // Do some linear algebra with RcppArmadillo
+}
+
+// Dispatch function for someArmaFunction
+//
+// We need to write this wrapper to handle the different types used
+// when constructing a big.matrix
+//
 // [[Rcpp::export]]
-arma::mat matrixSubset(arma::mat M) {
-    // logical conditionL where is transpose larger?
-    arma::umat a = trans(M) > M;   
-    arma::mat  N = arma::conv_to<arma::mat>::from(a);
-    return N;
+void someArmaFunction(SEXP pBigMat) {
+  XPtr<BigMatrix> xpMat(pBigMat);
+
+  // Normally we would use the MatrixAccessor class to access the matrix
+  // associated with the external pointer. The matrix is stored as the
+  // `matrix` field of `xpMat`, and the MatrixAccessor simply provides
+  // convenient methods for accessing its elements.
+  //
+  // This is just a pointer to an array, which is laid out in memory in
+  // the column major format. Armadillo matrices are also stored in column
+  // major format. We can therefore use the advanced `arma::mat` constructor
+  // with `copy_aux_mem` set to `false` to effectively "cast" this memory
+  // to an object RcppArmadillo understands.
+  //
+  // Warning about safety and accessing the memory appropriately, along
+  // with the advanced option of `strict=true` if you want to modify the
+  // size of the matrix later.
+  //
+  unsigned int type = xpMat->matrix_type();
+  if (type == 1) {
+    someArmaFunction<char>(
+      arma::Mat<char>((char *)xpDat->matrix(), xpDat->nrow(), xpDat->ncol(), false)
+    );
+  } else if (type == 2) {
+    someArmaFunction<short>(
+      arma::Mat<short>((short *)xpDat->matrix(), xpDat->nrow(), xpDat->ncol(), false)
+    );
+  } else if (type == 4) {
+    someArmaFunction<int>(
+      arma::Mat<int>((int *)xpDat->matrix(), xpDat->nrow(), xpDat->ncol(), false)
+    );
+  } else if (type == 8) {
+    someArmaFunction<double>(
+      arma::Mat<double>((double *)xpDat->matrix(), xpDat->nrow(), xpDat->ncol(), false)
+    );
+  } else {
+    throw Rcpp::exception("Undefined type for provided big.matrix");
+  }
 }
 
 /*** R
-M <- matrix(1:9, 3, 3)
-M
-matrixSubset(M)
-*/ 
-
-/**
- * This generalizes to other uses, and the vector or matrix of
- * unsigned ints can be used inside the `elem()` member function.
- * Here were we return all values of `M * M'` that are greater or
- * equal to 100.   
- *
- */
-
-// [[Rcpp::export]]
-arma::vec matrixSubset2(arma::mat M) {
-    arma::mat Z = M * M.t();
-    arma::vec v = Z.elem( arma::find( Z >= 100 ) );
-    return v;
-}
-
-/**
- * The result:
- */
-
-/*** R
-matrixSubset2(M)
+M <- as.big.matrix(matrix(1:6, nrow=2))
+someArmaFunction(M@address)
 */
+
